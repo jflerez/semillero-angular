dea3da231368c1a7c7d78006fb11c600886af155
Jonathan Flerez <jhonatanflerez@gmail.com>
2020-01-30 00:40:14 -0500

    consumo de servicio para listar usuarios

--- src/app/components/pagina-principal/pagina-principal.component.ts
+++ src/app/components/pagina-principal/pagina-principal.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PaginaPrincipalService } from "../../services/pagina-principal/pagina-principal.service";
+import { Usuario } from "../../dto/usuario";
 
 @Component({
-  selector: 'app-pagina-principal',
-  templateUrl: './pagina-principal.component.html',
-  styleUrls: ['./pagina-principal.component.scss']
+  selector: "app-pagina-principal",
+  templateUrl: "./pagina-principal.component.html",
+  styleUrls: ["./pagina-principal.component.scss"]
 })
 export class PaginaPrincipalComponent implements OnInit {
+  usuarios: Usuario;
 
-  constructor() { }
+  constructor(private principal: PaginaPrincipalService) {}
 
   ngOnInit() {
+    this.principal.paginaPrincipal().subscribe(
+      data => (this.usuarios = data),
+      err => {
+        console.log(err);
+      },
+      () => {
+        console.log("complete");
+      }
+    );
   }
-
 }
--- src/app/services/pagina-principal/pagina-principal.service.ts
+++ src/app/services/pagina-principal/pagina-principal.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Usuario } from "../../dto/usuario";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PaginaPrincipalService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  paginaPrincipal(){
-    this.http.get('');
+  paginaPrincipal(): Observable<Usuario> {
+    return this.http.get<Usuario>(
+      "http://my-json-server.typicode.com/brayan1119/curso-angular/usuarios"
+    );
   }
 }
